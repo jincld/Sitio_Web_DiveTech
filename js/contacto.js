30508c6f0617a2ff39e10095d77fef9f130cd481
Jonathan <Jonathan>
2024-03-12 21:14:29 -0600

    Contactanos Terminado

---
+++ js/contacto.js
@@ -0,0 +1,8 @@
+enviarBtn.addEventListener('click', function(){
+    Swal.fire({
+    icon:'success',
+    title:'Mensaje enviado',
+    text:'Muchas gracias por sus comentarios.',
+    confirmButtonText:'Entendido'
+  });
+});
